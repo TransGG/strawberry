8127f4d7b1690fb05b22019fdb93bb53b7785e98
elise32 <27158829+elise32@users.noreply.github.com>
2022-07-28 19:05:33 -0500

    Added welcome mesesage

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const { Client, GatewayIntentBits, Partials, MessageFlags } = require('discord.js')
+const { Client, GatewayIntentBits, Partials, MessageFlags, MessageManager } = require('discord.js')
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
     ]
 })
 
@@ -16,8 +17,14 @@ client.on('ready', () => {
 client.on('messageCreate', message => {
     console.log('msg content: ', message.content)
     if (message.content.toLocaleLowerCase() === 'hi'){
-        message.reply(`U/n: ${message.author.username} ID: ${message.author.id} Tag: ${message.author.tag}`)
+        message.reply(`U/n: ${message.author.username} ID: <@${message.author.id}> Tag: ${message.author.tag}`)
     }
+})
+
+const welcomeChannelId = '987229949633105982'
+
+client.on('guildMemberAdd', (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
 })
 
 client.login(process.env.TOKEN)
