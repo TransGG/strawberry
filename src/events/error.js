1c804275bac04b15579dd7edfa4b360f9f27182c
elise32 <27158829+elise32@users.noreply.github.com>
2022-10-21 23:09:27 -0500

    add error event handler (experimental)

---
+++ src/events/error.js
@@ -0,0 +1,24 @@
+import Event from '../Event.js';
+
+/**
+ * Handler for error event
+ */
+class Error extends Event {
+    /**
+     * @param {Client} client The Discord Client that will handle this interaction
+     * @param {String} name The name of this interaction
+     */
+    constructor(client, name = 'error') {
+        super(client, name);
+    }
+
+    /**
+     * @param {Error} error The error that was encountered
+     */
+    async run(error) {
+        console.log('In error handler:');
+        console.error(error);
+    }
+}
+
+export default Error;
