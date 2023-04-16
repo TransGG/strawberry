4e0eaaf528dd4fc4aced7d6d71c10fdd8c202174
Cleo <cleo.debeau@gmail.com>
2023-04-16 18:36:50 -0400

    Added New Account Reminder + Inactive Reminder

--- src/config/config.js
+++ src/config/config.js
@@ -99,9 +99,9 @@ const devServ = {
         verifier: '1046271388782186590',
         verified: '1092270181012742144',
         greeter: '1097204070252548097',
+        inactivityPing: '1046271388782186590',
     },
 };
-
 
 const production = {
     debugOut: () => { },
@@ -136,6 +136,7 @@ const production = {
         verified: '959748411844874240',
         newbie: '1085261027807088710',
         greeter: '978861945253945394',
+        inactivityPing: '1097289190682660884',
     },
 
 };
