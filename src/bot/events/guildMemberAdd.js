fdecc573d8e0a917f32be915326ef647df691e03
Cleo <cleo.debeau@gmail.com>
2024-06-27 16:40:26 -0300

    Added check on join to see if they are verified in any synced communities

--- src/bot/events/guildMemberAdd.js
+++ src/bot/events/guildMemberAdd.js
@@ -56,6 +56,53 @@ Permanent invite link: https://discord.gg/${config.guilds[member.guild.id].invit
             config.guilds[member.guild.id].roles.member,
             'Auto adding member role.',
         );
+
+        // Check if they are verified in any of the linked communities
+
+        config.guilds[member.guild.id].sync.forEach(async (guildId) => {
+            const guild = this.client.guilds.cache.get(guildId);
+
+            if (!guild) {
+                return;
+            }
+
+            try {
+                const memberInGuild = await guild.members.fetch(member.id);
+                // check if that have that guilds verified role
+                const hasVerifiedRole = memberInGuild.roles.cache.has(
+                    config.guilds[guildId].roles.verified,
+                );
+
+                if (hasVerifiedRole) {
+                    member.roles.add(
+                        config.guilds[member.guild.id].roles.verified,
+                        `Verified in ${memberInGuild.guild.name} | Auto adding member role from synced community.`,
+                    );
+
+                    // Send a log in both servers verify-log channel
+                    const logChannel = member.guild.channels.cache.get(
+                        config.guilds[member.guild.id].channels.verifyLogs,
+                    );
+                    const logChannel2 = guild.channels.cache.get(
+                        config.guilds[guildId].channels.verifyLogs,
+                    );
+
+                    if (logChannel) {
+                        logChannel.send({
+                            content: `<@${member.user.id}> (${member.user.id}) is already verified in \`${guild.name}\` (${guild.id}). They have been auto verified.`,
+                        });
+                    }
+
+                    if (logChannel2) {
+                        logChannel2.send({
+                            content: `<@${member.user.id}> (${member.user.id}) has joined \`${member.guild.name}\` (${member.guild.id}) and has been auto verified due to being verified in this community.`,
+                        });
+                    }
+                }
+            } catch {
+                console.log(`Failed to fetch member in ${guild.name}, user likely isn't in guild.`);
+            }
+        });
     }
 }
 
