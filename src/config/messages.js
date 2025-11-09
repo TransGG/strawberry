3c40793caabe1526509d7efbcc0929e6bba6d2ad
pat-5621-me <pat.5621.me@gmail.com>
2025-11-09 18:14:50 -0500

    GA rules update (#58)
    * GA rules update
    
    overhauled the GA rules to rid of the unnecessary duplicates and condense them into a more user-friendly readable package.
    
    * Update messages.js
    
    corrected rules pr
    
    * fix discrepancies with #rules
    
    ---------
    
    Co-authored-by: hyperneutrino <me@hyperneutrino.xyz>

--- src/config/messages.js
+++ src/config/messages.js
@@ -65,7 +65,7 @@ const TPRulesEmbed = {
 
 const notesReportEmbed = {
     color: 0xDF585B,
-    title: 'Notes / Report to staff',
+    title: 'Notes / Report to Staff',
     description: '**Please be the bigger person**—if you see someone trying to start a fight, don\'t fight back; DM staff. Similarly, if you see anything that may cause issues or someone possibly willingly causing them, don’t try to argue them into submission. *Don’t feed the trolls, nor your own trauma responses*.\n\nIf you see something against the rules or something that makes you feel unsafe, let staff know. We want this space to be as inclusive and safe as possible. \n\n**To do this:**\n`Right-Click A Message > Apps > Report Message`\n\n***This directly reports the message to our server staff for us to best handle the situation as fast as possible <3 ***\n\n> *This does not report the message to discord, just to our server staff.*',
     image: { url: 'https://raw.githubusercontent.com/TransGG/assets/refs/heads/main/how-to-report.gif' },
     footer: { text: 'If you are ever unsure if something is allowed, feel free to ask.' },
@@ -83,80 +83,52 @@ const GARulesEmbed1 = {
     title: 'Rules',
     fields: [
         {
-            name: '`1.` Be civil and respectful.',
-            value: 'Treat others with kindness and respect at all times. Avoid offensive behavior, personal attacks, and disrespectful language. Disagreements are acceptable, but keep discussions constructive and avoid being overly confrontational. No slurs in ANY way. [Read More]({{rule1}})',
+            name: '`1.` Hate Has No Home Here.',
+            value: '> Treat others with kindness and respect at all times. Avoid offensive behavior, personal attacks, and disrespectful language. Bigotry, slurs, harassment, hateful politics, or any other form of hate are not tolerated.',
         },
         {
             name: '`2.` No spam or self promotion.',
-            value: 'Do not flood the server with irrelevant or repetitive messages. Self-promotion, such as sharing links or content related to external products or services, is only allowed if explicitly approved by the server administrators. \nThis includes the promotion or offering invites to other discord servers. Even if you yourself don\'t own it. It still is promoting another discord server, and forbidden.',
+            value: '> Spam or self-promotion, such as sharing links or content related to external products or services, is only allowed if explicitly approved by the server administrators. This includes links to other discord servers and anything that could reasonably be considered spam.',
         },
         {
             name: '`3.` No Doxing or Sharing Personal Info.',
-            value: 'Sharing personal information of any user without their consent is strictly prohibited. This includes real names, addresses, contact details, or any other private information.',
+            value: '> Sharing personal information of any user without their consent is strictly prohibited. This includes names, addresses, contact details, or any other private information. Doing so with intent is grounds for a ban on all partnered servers.',
+        },
+        {
+            name: '`4.` Limit Political Discussions',
+            value: '> Our community is focused on keeping people safe, but we also want people to exercise their freedom of expression. To that end, especially because we are an explicitly anarchist server and because LGBTQIA+ and especially trans people have been politicized so much, political discussion is allowed. \n> Bigoted topics are banned. For a complete list, [click here](https://canary.discord.com/channels/1135300957572431902/1246845823950655498/1246846431332008008). \n> Staff reserve the right to shut down any heated discussion by any means necessary for the mood of the server.',
         },
         {
             name: '`5.` No Breaking Discord TOS or Community Guidelines.',
-            value: 'Being under 13 is [not allowed]({{rule5}})',
+            value: '> Being under 13 is [not allowed](https://support.discord.com/hc/en-us/articles/360040724612-Why-is-Discord-asking-for-my-birthday). Furthermore discussions or sharing of any illegal content, including but not limited to malicious software, illegal drugs, or any other illicit activities, are also not allowed in the server.\n[Discord TOS](https://dis.gd/tos) and [Community Guidelines](https://dis.gd/guidelines).',
         },
         {
-            name: '`6.` Absolutely No Bigotry.',
-            value: 'There is a zero-tolerance policy towards any form of bigotry. This includes transphobia, homophobia, racism, sexism, and any discriminatory attitudes towards individuals based on their identity or orientation. [Read More]({{rule6}})',
+            name: '`6.` Respect plural members.',
+            value: '> Respect plural members, and only use PK for our allowed use cases. If you see users talking with the bot tag, they\'re talking through PluralKit. Due to Discord limitations, these messages will show up with the [BOT] tag - however, they are not from bots, they are from users. Additionally, PluralKit is not for any form of roleplay. Any form of discrimination or hate against alters will be met with a ban. all forms and origins of plurality are accepted here. ***All users of an account are responsible for its conduct. This includes plural users. It is up to the collective users of an account to be responsible for what happens on their account.***',
         },
         {
-            name: '`7.` No Pedophilia Apologism.',
-            value: 'Under no circumstances should pedophilia be normalized or defended. This includes any attempts to compare it favorably to non-heterosexual sexualities or discussions involving drawn/animated child pornography.',
+            name: '`7.` Do not attempt to bypass the Automod.',
+            value: '> This server uses auto-mod to block messages deemed NSFW, violent, or triggering. Do not attempt to circumvent the Automod.',
         },
         {
             name: '`8.` Age-Appropriate Content Only.',
-            value: 'To ensure a safe and welcoming space for all, please avoid sharing any suggestive or explicit content, as well as engaging in explicit discussions. We have minors in our community, and such content shouldn\'t be seen by minors. Absolutely 0 NSFW content should be included in your profile.  [Read More]({{rule8}})',
+            value: '> To ensure a safe and welcoming space for all, do not share any suggestive or explicit content, or engage in explicit discussions; even in the mature channels. We have minors in our community, and such content shouldn\'t be seen by minors. Absolutely no NSFW content can be included in your profile or on this server. [Read More](https://canary.discord.com/channels/1135300957572431902/1435465648498016276)',
         },
         {
             name: '`9.` Please keep all channels on-topic.',
-            value: 'Please keep topics in a channel within the general theme of the channel. This includes venting staying within the venting channels. Topics about ||eating disorders, suicide, and self harm,|| are strictly prohibited for the safety of members.',
+            value: '> Please keep topics in a channel within the general theme of the channel. This includes venting staying within the venting channels. Topics about ||eating disorders, suicide, and self harm,|| and the like are strictly prohibited for the safety of members.',
         },
         {
-            name: '`10.` No spam.',
-            value: 'Please do not spam within the server. This includes spam pinging members and roles.',
-        },
-    ],
-    image: { url: 'https://raw.githubusercontent.com/TransGG/assets/refs/heads/main/embed-sizer.png' },
-};
-
-const GARulesEmbed2 = {
-    color: 0xDF585B,
-    title: 'Rules pt.2 Electric Boogaloo',
-    fields: [
-        {
-            name: '`11.` No mini-modding.',
-            value: 'It gets in the way of mods doing our job. If you\'re not a mod, you do not have the power to moderate effectively. [Example of mini-modding and how it\'s ineffective on the Hypixel forums.]({{rule11}})',
+            name: '`10.` Avoid mini-modding.',
+            value: '> If you see rules being broken, please use our custom report `[Example Below]` feature or open a support ticket instead of taking matters into your own hands. Our staff team volunteer their time to ensure a positive environment, and their decisions are always made with the community\'s best interests at heart. If you ever have concerns regarding a decision we\'ve made, we welcome you to open a ticket.',
         },
         {
-            name: '`12.` Do not impersonate anyone.',
-            value: 'It can cause others to become uncomfortable. Additionally users should be allowed to control their own image. As one of two exceptions on this list, if you\'d like to for a joke, please ASK that member first!',
+            name: '`11.` Keep all conversations in English.',
+            value: '> Short phrases or jokes in another language are allowed, but we cannot effectively moderate non-English extensive discussions.',
         },
         {
-            name: '`13.` Respect plural members.',
-            value: 'Respect plural members, and only use PK for our allowed use cases. If you see users talking with the bot tag, they\'re talking through PluralKit. Due to Discord limitations, these messages will show up with the [BOT] tag - however, they are not bots, they are users. Additionally, PluralKit is not for any form of roleplay. Any form of discrimination or hate against alters will be met with a ban, all forms and origins of plurality are accepted here. ***All users of an account are responsible for its conduct. This includes plural users. It is up to the collective users of an account to be responsible for what happens on their account.***',
-        },
-        {
-            name: '`15.` Honesty.',
-            value: 'Please do not spread lies. This especially includes lying about your age, as doing so is a bannable offense . This has the exception of the <#1150588260729499778> channel, as long as they aren\'t actively harmful!',
-        },
-        {
-            name: '`16.` Keep all conversations in English.',
-            value: 'Short phrases or jokes in another language are allowed, but we cannot effectively moderate non-English extensive discussions. [Read More]({{rule16}})',
-        },
-        {
-            name: '`17.` Do not bypass auto-mod.',
-            value: 'This server uses auto-mod to block messages deemed nsfw, violent, or triggering. Do not circumvent the auto-mod. [Read More]({{rule17}})',
-        },
-        {
-            name: '`18.` Limit political discussion.',
-            value: 'Our community is focused on keeping our members safe, but we also want to give people freedom of discussion. To that end, especially because we are an explicitly anarchist server and because LGBTQIA+ and especially trans people have been politicized so much, political discussion is allowed.\n\nHOWEVER, that only extends to politics that are accepting of others and human rights. That means LGBTQIA+ politics, ACAB, trans rights, black lives matter, general leftist politics, etc. \n\nAny form of bigoted politics including nazism and general bigotry, cops, landlords, general conservativism, politics spouted by people such as Joe Rogan and Andrew Tate, tankies, and the like is banned. \n\nFor a full list of topics, please see [Read More]({{rule18}})\n\nNote that any political discussion that gets heated, even if it is leftist in nature, is also strictly banned for the safety and comfort of our members.',
-        },
-        {
-            name: '`19.` Moderators have discretion to do what they see fit.',
-            value: 'If you\'re misbehaving, but it\'s not against any single rule, mods may take any action they see fit, within reason. If you believe a moderator has used this to abuse their power, please #contact-staff.',
+            name: '`12.` Moderator Discretion',
+            value: '> Our staff has the discretion to do what they find necessary to ensure a safe server for all. Remember to follow the spirit of these rules, not just the letter.',
         },
     ],
     image: { url: 'https://raw.githubusercontent.com/TransGG/assets/refs/heads/main/embed-sizer.png' },
@@ -165,8 +137,8 @@ const GARulesEmbed2 = {
 const GAMentalHealthEmbed = {
     color: 0xDF585B,
     title: '🔴 IMPORTANT 🔴',
-    description: 'We are not mental health professionals. As much as we would like to be able to render assistance in every way possible, we as staff do not have the capacity or the professional qualifications necessary to render proper assistance with mental health issues, nor are we able to give professional advice. Thank you for understanding ❤️',
-    image: { url: 'https://raw.githubusercontent.com/TransGG/assets/refs/heads/main/embed-sizer.png' },
+    description: 'We are not mental health professionals. As much as we would like to be able to render assistance in every way possible, we as staff do not have the capacity or the professional qualifications necessary to render proper assistance with mental health issues, nor are we able to give professional, medical, or legal advice. Thank you for understanding ❤️',
+    image: { url: 'https://i.imgur.com/CBbbw0d.png' },
 };
 
 export const TPRulesMessages = [
@@ -175,8 +147,7 @@ export const TPRulesMessages = [
 ];
 
 export const GARulesMessages = [
-    { embeds: [GARulesEmbed1, GARulesEmbed2] },
-    { embeds: [notesReportEmbed, GAMentalHealthEmbed] },
+    { embeds: [GARulesEmbed1, notesReportEmbed, GAMentalHealthEmbed] },
 ];
 
 export const CDLFRulesMessages = [
